0afe74189a8fe214ef04449685b223dddc1999ce
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-29 10:04:00 +0300

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Defines the root path route ("/")
   # root "articles#index"
 end
